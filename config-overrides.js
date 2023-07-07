de10e7287267c01507cf28e74e7edb59f93388b8
Daniel Oiteiro <danieloiteiro@gmail.com>
2023-07-07 14:31:53 +0200

    Exclude 'mapbox v2.3.1

--- config-overrides.js
+++
@@ -1,6 +0,0 @@
-module.exports = function override(config, env) {
-    // Exclude 'mapbox-gl' from transpilation
-    config.module.rules[1].oneOf[2].exclude = /node_modules\/(?!(mapbox-gl)\/).*/;
-    return config;
-  };
-  
